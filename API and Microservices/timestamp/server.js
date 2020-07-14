fd68ef008d6218a1f234ba658d9874d356f24ac6
basedantoni <anthony.mercado300@gmail.com>
2020-07-13 19:49:37 -0500

    headparser added

--- API and Microservices/timestamp/server.js
+++ API and Microservices/timestamp/server.js
@@ -20,13 +20,35 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/timestamp/:date_string?", function (req, res) {
 
+    let date = req.params.date_string;
 
+    console.log('Date Parsed: ', Date.parse(date));
+    // Check if date is empty
+    if(date === '') {
+        console.log('Date is empty');
+        let newDate = Date.now();
+        let utcDate = new Date(newDate);
+        res.json({unix: newDate, utc: utcDate.toUTCString()});
+    }
+
+    // Check if date can be parsed
+    else if(Date.parse(date) !== Number && !date.indexOf('-')) {
+        console.log('Date can\'t be parsed');
+        res.json({error: 'Invalid Date'})
+    } 
+    else if(Date.parse(date) >= 0 || parseInt(date)){
+        console.log('Normal Date');
+        res.json({unix: date, utc: new Date(date).toUTCString()})
+    }
+    else if(Date.parse(date) < 0) {
+        console.log('Parsed Int', parseInt(date))
+        res.json({unix: date, utc: new Date(parseInt(date)).toUTCString()})
+    }
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000 || process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
