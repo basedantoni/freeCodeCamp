9e1e9141ea27ab8d92dcd436a66bfb7e928e78c1
basedantoni <anthony.mercado300@gmail.com>
2020-07-14 18:57:18 -0500

    primary functionality completed

--- API and Microservices/urlshortener/server.js
+++ API and Microservices/urlshortener/server.js
@@ -1,8 +1,11 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dns = require('dns');
+let Schema = mongoose.Schema;
 
 var cors = require('cors');
 
@@ -20,18 +23,55 @@ app.use(cors());
 // you should mount the body-parser here
 
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
   
+// ShortURL Schema
+let ShortURL = new Schema({
+    original: String,
+    shortend: Number
+})
+
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.post('/api/shorturl/new', (req, res) => {
+    const url = req.body.url;
+    const invalidUrl = {error: 'Invalid URL'};
+
+    // Check if url is valid
+    if(url.includes('https://') || url.includes('http://')) {
+        const invalidHostname = {error: 'Invalid Hostname'};
+        const hostName = url.slice(url.indexOf('/') + 2);
+
+        let urlObj = {
+            original_url: String,
+            short_url: Number
+        }
+
+        console.log('Host: ', hostName);
+        let ipRegex = /^[23]/
+        dns.lookup(hostName, (err, address, family) => {
+            console.log('Address: ', address);
+
+            if(ipRegex.test(address)) {
+                console.log('hello');
+                res.json(invalidHostname);
+            } else {
+                urlObj.original_url = url;
+                urlObj.short_url = 1;
+                res.json(urlObj);
+            }
+
+            
+            err ? console.log(err) : console.log('Look Up Success');
+            family ? console.log('Family: ', family) : urlObj = invalidHostname; 
+        })
+    }
 });
 
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log('Node.js listening on port: ' + port);
 });
