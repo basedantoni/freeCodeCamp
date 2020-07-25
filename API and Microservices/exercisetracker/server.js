377d3a4fd6c7914744feeaa7b11e709b8cbec075
basedantoni <anthony.mercado300@gmail.com>
2020-07-25 00:50:30 -0500

    finished first three routes

--- API and Microservices/exercisetracker/server.js
+++ API and Microservices/exercisetracker/server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 
 const cors = require('cors')
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const { urlencoded } = require('body-parser');
 const Schema = mongoose.Schema;
 
 mongoose
@@ -15,13 +16,125 @@ mongoose
 
 app.use(cors())
 
+// body-parser
 app.use(express.json({extended: false}));
 
+// Using querystring
+app.use(urlencoded({extended: false}));
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+const userSchema = new Schema( {
+  username: String,
+  exercises: [Object]
+})
+
+const User = mongoose.model('user', userSchema);
+
+// @route POST api/exercise/new-user
+// @desc Register User
+// @access Public
+app.post('/api/exercise/new-user', (req, res) => {
+  const username = req.body.username
+
+  User.find({username: username}).then(doc => {
+    // Check if doc exists
+    if(doc.length) {
+      res.send('Username already taken')
+    } else {
+      let newUser = User({
+        username: username
+      })
+    
+      // Save new user
+      newUser
+        .save()
+        .then(doc => {
+          res.json({
+            username: doc.username,
+            _id: doc._id
+          })
+        })
+        .catch(err => console.log(err))
+    }
+  })
+})
+
+// @route GET api/exercise/users
+// @desc Get All Users
+// @access Public
+app.get('/api/exercise/users', (req, res) => {
+  User.find({}).then(docs => res.json(docs)).catch(err => console.log(err))
+})
+
+// @route POST api/exercise/add
+// @desc Register User
+// @access Public
+app.post('/api/exercise/add', (req, res) => {
+  let { userId, description, duration, date } = req.body;
+
+  // If date is empty
+  if(!date) {
+    console.log('NO DATE')
+    const newDate = Date.now();
+    const utcDate = new Date(newDate);
+    const dateString = utcDate.toDateString();
+
+    User.findOne({_id: userId}, (err, doc) => {
+      if(err) {
+        console.log(err)
+        res.send('UserId doesn\'t exist')
+      } else {
+        req.body.date = dateString;
+        doc.exercises.push(req.body);
+
+        doc
+          .save()
+          .then(doc => res.json({
+            _id: doc._id,
+            username: doc.username,
+            date: doc.date,
+            duration: doc.duration,
+            description: doc.description
+          }))
+          .catch(err => console.log(err))
+      }
+    })
+  } else {
+    console.log('YES DATE')
+    const utcDate = new Date(date);
+    const dateString = utcDate.toDateString();
+
+    User.findOne({_id: userId}, (err, doc) => {
+      if(err) {
+        console.log(err)
+        res.send('UserId doesn\'t exist')
+      } else {
+        doc.exercises.push(req.body);
+        doc
+          .save()
+          .then(doc => res.json({
+            _id: doc._id,
+            username: doc.username,
+            date: dateString,
+            duration: duration,
+            description: description
+          }))
+          .catch(err => console.log(err))
+      }
+    })
+  }
+})
+
+// @route GET api/exercise/log
+// @desc Get Exercise Log
+// @access Public
+app.get('/api/exercise/log', (req, res) => {
+  
+})
 
 
 // Not found middleware
@@ -32,6 +145,7 @@ app.use((req, res, next) => {
 // Error Handling middleware
 app.use((err, req, res, next) => {
   let errCode, errMessage
+  console.log(err)
 
   if (err.errors) {
     // mongoose validation error
@@ -46,44 +160,6 @@ app.use((err, req, res, next) => {
   }
   res.status(errCode).type('txt')
     .send(errMessage)
-})
-
-const userSchema = new Schema( {
-  username: String
-})
-
-const User = mongoose.model('user', userSchema);
-
-// @route POST api/exercise/new-user
-// @desc Register User
-// @access Public
-app.post('/api/exercise/new-user', (req, res) => {
-  let newUser = User({
-    username: req.body.username
-  })
-
-  newUser.save().then(doc => res.json(doc)).catch(err => console.log(err))
-})
-
-// @route GET api/exercise/users
-// @desc Get Users
-// @access Public
-app.get('/api/exercise/users', (req, res) => {
-  
-})
-
-// @route POST api/exercise/add
-// @desc Register User
-// @access Public
-app.post('/api/exercise/add', (req, res) => {
-
-})
-
-// @route GET api/exercise/log
-// @desc Get Exercise Log
-// @access Public
-app.get('/api/exercise/log', (req, res) => {
-  
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
